3cc0e86db32b03302ba27d248621658359e912ae
ana-staa-siaa <anastasiakharitonova94@gmail.com>
2026-03-06 01:24:06 +0300

    2 task

---
+++ replace_by_dictionary.py
@@ -0,0 +1,63 @@
+import json
+import os
+import re
+
+def replace_terms_in_files(folder_path, terms_map):
+    terms_pattern = re.compile(
+        r'\b(' + '|'.join(map(re.escape, terms_map.keys())) + r')(?:\'s|s|es)?\b',
+        flags=re.IGNORECASE
+    )
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+
+            def replacer(match):
+                matched_word = match.group(0)
+                suffix = ''
+                base_word = matched_word
+
+                if matched_word.lower().endswith("'s"):
+                    suffix = "'s"
+                    base_word = matched_word[:-2]
+                elif matched_word.lower().endswith("es"):
+                    base_without_es = matched_word[:-2]
+                    if base_without_es.lower() in [term.lower() for term in terms_map.keys()]:
+                        suffix = "es"
+                        base_word = base_without_es
+                    else:
+                        base_word = matched_word  # 's' - часть основного слова
+
+                elif matched_word.lower().endswith("s"):
+                    base_without_s = matched_word[:-1]
+                    if base_without_s.lower() in [term.lower() for term in terms_map.keys()]:
+                        suffix = "s"
+                        base_word = base_without_s
+                    else:
+                        base_word = matched_word  # 's' - часть основного слова
+
+                for term, new_term in terms_map.items():
+                    if term.lower() == base_word.lower():
+                        return new_term + suffix
+                return matched_word
+
+            new_content = terms_pattern.sub(replacer, content)
+
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(new_content)
+            print(f"Обработан файл: {filename}")
+
+def main():
+    folder_path = "knowledge_base"
+    terms_map_path = os.path.join("terms.json")
+
+    with open(terms_map_path, 'r', encoding='utf-8') as file:
+        terms_map = json.load(file)
+
+    replace_terms_in_files(folder_path, terms_map)
+    print("Замены выполнены успешно!")
+
+if __name__ == "__main__":
+    main()
